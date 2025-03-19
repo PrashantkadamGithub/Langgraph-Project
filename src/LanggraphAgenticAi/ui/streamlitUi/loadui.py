196a750784fd499f24b5de0c0ccdce4be36e4ea9
PrashantkadamGithub <126754599+PrashantkadamGithub@users.noreply.github.com>
2025-03-19 14:59:58 +0530

    Update loadui.py

--- src/LanggraphAgenticAi/ui/streamlitUi/loadui.py
+++ src/LanggraphAgenticAi/ui/streamlitUi/loadui.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import os
 from datetime import date
-
-from langchain_core.messages import AIMessage,HumanMessage
+from langchain_core.messages import AIMessage, HumanMessage
 from src.LanggraphAgenticAi.ui.uiconfigfile import Config
 
 
